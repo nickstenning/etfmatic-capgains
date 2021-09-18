3670baa2835c4763f09d971f77adc7ab5c71d0ae
Nick Stenning <nick@whiteink.com>
2021-09-18 15:29:13 +0200

    Improve format of capital gains reporter
    Switch to generating one year's data at a time

--- capgains.py
+++ capgains.py
@@ -8,9 +8,12 @@ from decimal import *
 parser = argparse.ArgumentParser(description="Print capital gains summary")
 parser.add_argument('database',
                     help="Path to database")
+parser.add_argument('year',
+                    help="Year for which to calculate capital gains")
 
+D = Decimal
 QTY_FACTOR = 100000
-                    
+
 def iter_results(cur):
     while True:
         batch = cur.fetchmany()
@@ -29,20 +32,21 @@ def find_year_dividends(cur, year):
     """, (str(year),))
     return iter_results(cur)
 
-    
+
 def find_year_sales(cur, year):
     cur.execute("""
         select
             sales.id as sale_id,
             sales.date as sale_date,
             sales.isin as sale_isin,
+            sales.name as sale_name,
             sales.quantity as sale_quantity,
             sales.price as sale_price,
             sales.total as sale_total,
             reconciliation.quantity as reconciliation_quantity,
             purchases.id as purchase_id,
             purchases.date as purchase_date,
-            purchases.isin as purchase_isin, 
+            purchases.isin as purchase_isin,
             purchases.quantity as purchase_quantity,
             purchases.price as purchase_price,
             purchases.total as purchase_total
@@ -55,62 +59,68 @@ def find_year_sales(cur, year):
     """, (str(year),))
     return iter_results(cur)
 
-def qty(quantity):
-    return Decimal(quantity) / QTY_FACTOR
 
-def capgains_summary(conn, year):
+def qty(quantity):
+    return D(quantity) / QTY_FACTOR
+
+
+def single_sale_summary(sale_total, purchase_total):
+    print()
+    print(f"    Sale total            = {sale_total:12.4f}")
+    print(f"    Purchase total        = {purchase_total:12.4f}")
+    print(f"    Net profit on sale    = {sale_total - purchase_total:12.4f}")
+
+
+def dividends_summary(conn, year):
     cur = conn.cursor()
 
-    year_profit = Decimal(0)
+    year_dividends = D(0)
+
+    dividends = list(find_year_dividends(cur, year))
+
+    for d in dividends:
+        print(f"{d['date']}: {d['amount']}")
+        year_dividends += D(d['amount'])
+
+    return year_dividends
+
+
+def sales_profit_summary(conn, year):
+    cur = conn.cursor()
+
+    year_profit = D(0)
     purchase_total = None
     current_sale = None
     current_sale_total = None
 
-    dividends = list(find_year_dividends(cur, year))
     sales = list(find_year_sales(cur, year))
 
-    if not sales and not dividends:
-        return
-
-    print(f"YEAR {year}")
-    print("#########")
-
-    for d in dividends:
-        print(f"Dividend ({d['date']}): {d['amount']}")
-        year_profit += Decimal(d['amount'])
-   
-
     if not sales:
-        return
+        return year_profit
 
     for e in sales:
         if e['sale_id'] != current_sale:
             if current_sale is not None:
-                sale_profit = current_sale_total - purchase_total
-                print(f"    Purchase total: {purchase_total}")
-                print(f"    Net profit: {current_sale_total} - {purchase_total} = {sale_profit}")
-                year_profit += sale_profit
+                single_sale_summary(current_sale_total, purchase_total)
+                year_profit += current_sale_total - purchase_total
 
-            purchase_total = Decimal(0)
+            purchase_total = D(0)
             current_sale = e['sale_id']
-            current_sale_total = -Decimal(e['sale_total'])
+            current_sale_total = -D(e['sale_total'])
 
-            print(f"Sell ({e['sale_date']}): {qty(e['sale_quantity'])} units of {e['sale_isin']} @ {e['sale_price']}, total {e['sale_total']}")
+            print()
+            print(f"Sale of {e['sale_name']} (ISIN {e['sale_isin']})")
+            print(f"    Sell ({e['sale_date']}): {qty(e['sale_quantity']):19.5f} units @ {D(e['sale_price']):12.4f} = {D(e['sale_total']):12.4f}")
 
-        reconciled_proportion = round(Decimal(e['reconciliation_quantity'])/Decimal(e['purchase_quantity']), 2)
-        reconciled_total = reconciled_proportion * Decimal(e['purchase_total'])
+        reconciled_proportion = round(D(e['reconciliation_quantity'])/D(e['purchase_quantity']), 2)
+        reconciled_total = reconciled_proportion * D(e['purchase_total'])
         purchase_total += reconciled_total
-        print(f"    Buy ({e['purchase_date']}): {qty(e['reconciliation_quantity'])}/{qty(e['purchase_quantity'])} units @ {e['purchase_price']}, total {e['purchase_total']} (reconciled total {reconciled_total})")
-    
-    sale_profit = current_sale_total - purchase_total
-    print(f"    Purchase total: {purchase_total}")
-    print(f"    Net profit on sale: {current_sale_total} - {purchase_total} = {sale_profit}")
-    year_profit += sale_profit
+        print(f"    Buy  ({e['purchase_date']}): {qty(e['reconciliation_quantity']):9.5f}/{qty(e['purchase_quantity']):9.5f} units @ {D(e['purchase_price']):12.4f} = {reconciled_total:12.4f}")
 
-    print()
-    print(f"GRAND TOTAL PROFIT ({year}) = {year_profit}")
-    print()
-    print()
+    single_sale_summary(current_sale_total, purchase_total)
+    year_profit += current_sale_total - purchase_total
+
+    return year_profit
 
 def main():
     args = parser.parse_args()
@@ -125,9 +135,24 @@ def main():
     getcontext().traps[FloatOperation] = True
 
 
-    
-    for year in range(int(start), int(end)+1):
-        capgains_summary(conn, year)
+    print(f"CAPITAL GAINS SUMMARY, {args.year}")
+    print("###########################")
+    print()
+
+    print("Dividends")
+    print("---------")
+    dividends = dividends_summary(conn, args.year)
+
+    print()
+    print("Sales")
+    print("-----")
+    sale_profit = sales_profit_summary(conn, args.year)
+
+    print()
+    print(f"Total dividends           = {dividends:12.4f}")
+    print(f"Total sales profit        = {sale_profit:12.4f}")
+    print(f"GRAND TOTAL CAPITAL GAINS = {dividends + sale_profit:12.4f}")
+
 
 if __name__ == "__main__":
     main()
